395df4bdc7c5aa799dceca10dfbe5491c4743d69
Kazumasa Kohtaka <kkohtaka@gmail.com>
2021-09-10 02:46:21 +0900

    Add CRD `Child` for testing purpose

--- main.go
+++ main.go
@@ -93,6 +93,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Parent")
 		os.Exit(1)
 	}
+	if err = (&testcontrollers.ChildReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Child")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
